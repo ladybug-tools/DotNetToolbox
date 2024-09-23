c576a923325925551cc12b7b136a2f895265f3cd
Mingbo Peng <mingo1214@gmail.com>
2024-09-23 22:49:25 +0800

    fix(TS): support TransformDecorator

--- SchemaGenerator/TemplateModels/TypeScript/PropertyTemplateModel.cs
+++ SchemaGenerator/TemplateModels/TypeScript/PropertyTemplateModel.cs
@@ -5,6 +5,8 @@ using NJsonSchema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
 
 namespace TemplateModels.TypeScript;
 
@@ -25,25 +27,68 @@ public class PropertyTemplateModel : PropertyTemplateModelBase
     public bool HasValidationDecorators => ValidationDecorators.Any();
     public List<string> ValidationDecorators { get; set; }
 
+    public bool HasTransformDecorator => !string.IsNullOrEmpty(TransformDecorator);
+    public string TransformDecorator {  get; set; }
+
     public PropertyTemplateModel(string name, JsonSchemaProperty json) : base(name, json)
     {
 
-
         DefaultCodeFormat = ConvertTsDefaultValue(json);
-
 
         // check types
         Type = GetTypeScriptType(json, AddTsImportTypes);
-
 
         ConvertToJavaScriptCode = $"data[\"{PropertyName}\"] = this.{PropertyName};";
         ConvertToClassCode = $"this.{PropertyName} = obj.{PropertyName};";
         //validation decorators
         ValidationDecorators = GetValidationDecorators(json);
 
+        // get @Transform
+        TransformDecorator = GetTransform(json, false);
 
     }
+    public static string GetTransform(JsonSchema json, bool isArray)
+    {
+        var code = string.Empty;
+        if ((json.AnyOf?.Any()).GetValueOrDefault())
+        {
+            var allRefs = json.AnyOf.All(_ => _.HasReference);
+            if (!allRefs)
+                return code;
 
+            var refTypes = json.AnyOf.Select(r => r.ActualSchema.Title).ToList();
+
+            var tps = refTypes.Select(_ => $"if (item.type === '{_}') return {_}.fromJS(item);").ToList();
+            tps = tps.Take(1).Concat(tps.Skip(1).Select(_ => $"else {_}")).ToList();
+            tps.Add("else return item;");
+            tps = tps.Select(_ => $"      {_}").ToList();
+
+            var trans = string.Join(Environment.NewLine, tps);
+            code = $@"
+    @Transform(({{ value }}) => {{
+      const item = value;
+{trans}
+    }}))";
+            return isArray ? trans : code;
+        }
+        else if (json.IsArray)
+        {
+            var arrayItem = json.Item;
+            var itemCode = GetTransform(arrayItem, true);
+            if (string.IsNullOrEmpty(itemCode))
+                return code;
+            code = $@"
+    @Transform(({{ value }}) => value.map((item: any) => {{
+      {itemCode}
+    }}))";
+            return code;
+        }
+        else
+        {
+            return code;
+        }
+
+    }
 
 
     public static string GetTypeScriptType(JsonSchema json, Action<string> collectImportType)
