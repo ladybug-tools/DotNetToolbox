cfd8d3b28552b2bc8ae05421cf90c02c24a76345
Mingbo Peng <mingo1214@gmail.com>
2024-11-26 17:43:53 +0800

    fix(ClassTemplateModelBase): remove redundant code

--- SchemaGenerator/TemplateModels/Base/ClassTemplateModelBase.cs
+++ SchemaGenerator/TemplateModels/Base/ClassTemplateModelBase.cs
@@ -15,7 +15,7 @@ public class ClassTemplateModelBase
     public string Discriminator { get; set; }
     public string BaseDiscriminator { get; set; } // type reference keyword: "type"
 
-    public ClassTemplateModelBase(OpenApiDocument doc, JsonSchema json)
+    public ClassTemplateModelBase(JsonSchema json)
     {
         Description = json.Description?.Replace("\n", "\\n")?.Replace("\"", "\"\"");
         BaseDiscriminator = json.Discriminator;
