c2b67098ac91a738df43955826c80037d47f9ee2
Mingbo Peng <mingo1214@gmail.com>
2025-02-11 14:38:09 +0800

    fix(Helper): fix tests with local openapi sample files

--- Generator.Tests/CSharp/SimulationParamTests.cs
+++ Generator.Tests/CSharp/SimulationParamTests.cs
@@ -1,7 +1,5 @@
 using NSwag;
 using NJsonSchema;
-using NJsonSchema.CodeGeneration;
-using SchemaGenerator;
 using TemplateModels.CSharp;
 
 namespace Generator.Tests.CSharp
@@ -9,16 +7,12 @@ namespace Generator.Tests.CSharp
     public class SimulationParamTests
     {
 
-        static string workingDir = Environment.CurrentDirectory;
-        static string rootDir = workingDir.Substring(0, workingDir.IndexOf("src"));
         static OpenApiDocument doc = null;
         [SetUp]
         public void Setup()
         {
 
-            Console.WriteLine($"Current working dir: {workingDir}");
-            var docDic = Path.Combine(rootDir, ".openapi-docs");
-            var jsonFile = Path.Combine(docDic, "simulation-parameter_inheritance.json");
+            var jsonFile = Path.Combine(Helper.HoneybeeDir, "simulation-parameter_inheritance.json");
 
             var json = File.ReadAllText(jsonFile);
             doc = OpenApiDocument.FromJsonAsync(json).Result;
