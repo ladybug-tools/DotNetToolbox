282fafd5151cb30cb716505631ef4a7008d7e667
Mingbo Peng <mingo1214@gmail.com>
2025-02-11 14:57:05 +0800

    test(CleanName): test parameter name

---
+++ Generator.Tests/CSharp/CleanNames.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework.Internal;
+
+namespace Generator.Tests.CSharp
+{
+    public class CleanNames
+    {
+        [Test]
+        public void TestModelEditorRevitSDKParameterNames()
+        {
+            //parameter name RoomExportByIdSetting
+            var clean = TemplateModels.Helper.CleanName(TemplateModels.Helper.ToCamelCase("RoomExportByIdSetting"));
+            Assert.That(clean, Is.EqualTo("roomExportByIdSetting"));
+
+        }
+    }
+}
