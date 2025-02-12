b93fbf7d360905ca9a60d701e02f58020cef0e6e
Mingbo Peng <mingo1214@gmail.com>
2025-02-13 00:18:50 +0800

    fix(TemplateModel): add MethodTemplateModelBase

---
+++ SchemaGenerator/TemplateModels/Base/MethodTemplateModelBase.cs
@@ -0,0 +1,31 @@
+﻿using NSwag;
+using System.Linq;
+
+namespace TemplateModels.Base;
+
+public class MethodTemplateModelBase
+{
+    public string MethodName { get; set; }
+    public bool HasReturn { get; set; }
+    public bool HasParameter { get; set; }
+
+    public string ReturnDoc { get; set; }
+
+    public string Summary { get; set; }
+    public string Document { get; set; }
+
+    public string ReturnTypeName { get; set; } // void or type name
+    //public PropertyTemplateModelBase ReturnType { get; set; }
+    //public List<PropertyTemplateModelBase> Params { get; set; }
+
+    public MethodTemplateModelBase(string name, OpenApiPathItem openApi)
+    {
+        this.MethodName = Helper.CleanMethodName(name);
+        var operation = openApi.First().Value;
+        this.Summary = operation.Summary;
+        this.Document = operation.Description;
+
+
+    }
+
+}
